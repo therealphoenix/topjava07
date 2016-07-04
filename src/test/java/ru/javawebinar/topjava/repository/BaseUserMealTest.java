bc364fe4d8b27ce6ac11048ae665f69fec6e131c
therealphoenix <fennya@gmail.com>
2016-07-04 17:36:53 +0300

    HW5noOptional

--- src/test/java/ru/javawebinar/topjava/repository/BaseUserMealTest.java
+++ src/test/java/ru/javawebinar/topjava/repository/BaseUserMealTest.java
@@ -1,23 +1,16 @@
-package ru.javawebinar.topjava.service;
-
+package ru.javawebinar.topjava.repository;
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.repository.*;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.rules.Stopwatch;
 import org.junit.runner.Description;
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.javawebinar.topjava.MealTestData;
-import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.UserMeal;
-import ru.javawebinar.topjava.repository.BaseUserMealTest;
+import ru.javawebinar.topjava.service.UserMealService;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDate;
@@ -26,35 +19,19 @@ import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.MealTestData.MEAL1;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
-@ContextConfiguration({
-        "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db.xml"
-})
-@RunWith(SpringJUnit4ClassRunner.class)
-@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+/**
+ * Created by Hp on 03.07.2016.
+ */
 @ActiveProfiles(Profiles.ACTIVE_DB)
-public class UserMealServiceTest extends BaseUserMealTest {
-    private static final Logger LOG = LoggerFactory.getLogger(UserMealServiceTest.class);;
-
+ public abstract  class BaseUserMealTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
-    @Rule
-    // http://stackoverflow.com/questions/14892125/what-is-the-best-practice-to-determine-the-execution-time-of-the-bussiness-relev
-    public Stopwatch stopwatch = new Stopwatch() {
-        private void logInfo(Description description, long nanos) {
-            LOG.info(String.format("+++ Test %s spent %d ms",
-                    description.getMethodName(), TimeUnit.NANOSECONDS.toMillis(nanos)));
-        }
 
-        @Override
-        protected void finished(long nanos, Description description) {
-            logInfo(description, nanos);
-        }
-    };
 
     @Autowired
     protected UserMealService service;
--- src/test/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserRepositoryImplTest.java
+++ src/test/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserRepositoryImplTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.BaseUserTest;
 import ru.javawebinar.topjava.service.UserService;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
@@ -35,72 +36,7 @@ import static ru.javawebinar.topjava.UserTestData.*;
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 @ActiveProfiles(Profiles.ACTIVE_DB)
 
-public class DataJpaUserRepositoryImplTest {
-
-    @Autowired
-
-     protected UserService service;
-
-    @Before
-    public void setUp() throws Exception {
-        service.evictCache();
-    }
+public class DataJpaUserRepositoryImplTest extends BaseUserTest {
 
 
-
-    @Test
-    public void testSave() throws Exception {
-        TestUser tu = new TestUser(null, "New", "new@gmail.com", "newPass", 1555, false, Collections.singleton(Role.ROLE_USER));
-        User created = service.save(tu.asUser());
-        tu.setId(created.getId());
-        MATCHER.assertCollectionEquals(Arrays.asList(ADMIN, tu, USER), service.getAll());
-    }
-
-    @Test(expected = DataAccessException.class)
-    public void testDuplicateMailSave() throws Exception {
-        service.save(new TestUser("Duplicate", "user@yandex.ru", "newPass", Role.ROLE_USER).asUser());
-    }
-
-    @Test
-    public void testDelete() throws Exception {
-        service.delete(USER_ID);
-        MATCHER.assertCollectionEquals(Collections.singletonList(ADMIN), service.getAll());
-    }
-
-    @Test(expected = NotFoundException.class)
-    public void testNotFoundDelete() throws Exception {
-        service.delete(1);
-    }
-
-    @Test
-    public void testGet() throws Exception {
-        User user = service.get(USER_ID);
-        MATCHER.assertEquals(USER, user);
-    }
-
-    @Test(expected = NotFoundException.class)
-    public void testGetNotFound() throws Exception {
-        service.get(1);
-    }
-
-    @Test
-    public void testGetByEmail() throws Exception {
-        User user = service.getByEmail("user@yandex.ru");
-        MATCHER.assertEquals(USER, user);
-    }
-
-    @Test
-    public void testGetAll() throws Exception {
-        Collection<User> all = service.getAll();
-        MATCHER.assertCollectionEquals(Arrays.asList(ADMIN, USER), all);
-    }
-
-    @Test
-    public void testUpdate() throws Exception {
-        TestUser updated = new TestUser(USER);
-        updated.setName("UpdatedName");
-        updated.setCaloriesPerDay(330);
-        service.update(updated.asUser());
-        MATCHER.assertEquals(updated, service.get(USER_ID));
-    }
 }
