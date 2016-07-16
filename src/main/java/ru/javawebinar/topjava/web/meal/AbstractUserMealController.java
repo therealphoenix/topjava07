481c5c35ef024d67afaed0e6b72d9f339c5be234
therealphoenix <fennya@gmail.com>
2016-07-16 14:41:17 +0300

    patched_to_7-6

--- src/main/java/ru/javawebinar/topjava/web/meal/AbstractUserMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/AbstractUserMealController.java
@@ -2,34 +2,24 @@ package ru.javawebinar.topjava.web.meal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import ru.javawebinar.topjava.AuthorizedUser;
-import ru.javawebinar.topjava.model.UserMeal;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.service.UserMealService;
 import ru.javawebinar.topjava.to.UserMealWithExceed;
 import ru.javawebinar.topjava.util.TimeUtil;
 import ru.javawebinar.topjava.util.UserMealsUtil;
 
-import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * GKislin
  * 06.03.2015.
  */
-@Controller
-public class UserMealRestController extends AbstractUserMealController {
-    private static final Logger LOG = LoggerFactory.getLogger(UserMealRestController.class);
+public abstract class AbstractUserMealController {
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractUserMealController.class);
 
     @Autowired
     private UserMealService service;
@@ -45,13 +35,13 @@ public class UserMealRestController extends AbstractUserMealController {
         LOG.info("delete meal {} for User {}", id, userId);
         service.delete(id, userId);
     }
-//r
+
     public List<UserMealWithExceed> getAll() {
         int userId = AuthorizedUser.id();
         LOG.info("getAll for User {}", userId);
         return UserMealsUtil.getWithExceeded(service.getAll(userId), AuthorizedUser.getCaloriesPerDay());
     }
-//
+
     public void update(UserMeal meal, int id) {
         meal.setId(id);
         int userId = AuthorizedUser.id();
@@ -69,11 +59,10 @@ public class UserMealRestController extends AbstractUserMealController {
     public List<UserMealWithExceed> getBetween(LocalDate startDate, LocalTime startTime, LocalDate endDate, LocalTime endTime) {
         int userId = AuthorizedUser.id();
         LOG.info("getBetween dates {} - {} for time {} - {} for User {}", startDate, endDate, startTime, endTime, userId);
-
         return UserMealsUtil.getFilteredWithExceeded(
                 service.getBetweenDates(
-                        startDate != null ? startDate : TimeUtil.MIN_DATE, endDate != null ? endDate : TimeUtil.MAX_DATE, userId),
-                        startTime != null ? startTime : LocalTime.MIN, endTime != null ? endTime : LocalTime.MAX, AuthorizedUser.getCaloriesPerDay()
+                        startDate != null ? startDate : TimeUtil.MIN_DATE, endDate != null ? endDate : TimeUtil.MAX_DATE, userId
+                ), startTime != null ? startTime : LocalTime.MIN, endTime != null ? endTime : LocalTime.MAX, AuthorizedUser.getCaloriesPerDay()
         );
     }
 }
